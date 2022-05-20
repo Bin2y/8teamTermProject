a38836a372eb1e1abbe526d6da3a558b552a4211
KimGaeun309 <90085690+KimGaeun309@users.noreply.github.com>
2022-05-20 21:36:43 +0900

    Update loading.c

--- loading.c
+++ loading.c
@@ -13,52 +13,6 @@ unsigned int invertEndian(unsigned int data)
 	return *(unsigned int*)c;
 }
 
-
-// 바이너리 파일을 읽어서 memory에 loading 하고  PC, SP 를 초기화하는 함수
-void loading(char* bFileName) { 
-	FILE* bFile = nullptr;
-	errno_t err;
-
-	unsigned int data;
-	unsigned int addr;
-
-	unsigned int iCount; // # of instructions
-	unsigned int dCount; // # of data
-
-	err = fopen_s(&bFile, bFileName, "rb");
-	if (err) {
-		printf("Cannot open binary file\n");
-		return;
-	}
-
-	fread(&data, sizeof(data), 1, bFile);
-	iCount = invertEndian(data); 
-	fread(&data, sizeof(data), 1, bFile);
-	dCount = invertEndian(data);
-
-	resetMem();
-	
-	addr = PROG_START;
-	for (int i = 0; i < (int)iCount; i++) {
-		fread(&data, sizeof(unsigned int), 1, bFile);
-		data = invertEndian(data);
-		MEM(addr, data, WR, WORD);
-		addr += 4;
-	}
-
-	addr = DATA_START;
-	for (int i = 0; i < (int)dCount; i++) {
-		fread(&data, sizeof(unsigned int), 1, bFile);
-		data = invertEndian(data);
-		MEM(addr, data, WR, WORD);
-		addr += 4;
-	}
-
-	PC = PROG_START;
-	SP = STAK_END;
-
-}
-
 // memory.c 사용, register.c 사용 
 // 바이너리 파일을 읽어서 memory에 loading 하고  PC, SP를 초기화하는 함수
 void loading(char* bFileName) { 
