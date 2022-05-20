a2f69029e652b0b75583394331765bb982224654
KimGaeun309 <90085690+KimGaeun309@users.noreply.github.com>
2022-05-20 21:30:41 +0900

    Update loading.cpp

--- loading.cpp
+++ loading.cpp
@@ -1,91 +1,5 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include <iostream>
-
-#define PROG_START	0x00400000
-#define PROG_END	0x00500000
-#define DATA_START	0x10000000
-#define DATA_END	0x10100000
-#define STAK_START	0x7FF00000
-#define STAK_END	0x80000000
-
-unsigned int PC;
-unsigned int SP;
-
-const enum accessType { RD, WR };
-enum accessSize { BYTE, HALF, WORD };
-
-const int M_SIZE = 0x100000;
-unsigned char progMEM[M_SIZE], dataMEM[M_SIZE], stakMEM[M_SIZE];
-
-int MEM(unsigned int A, int V, int nRW, int S) {
-
-	unsigned int memSelect, offset; // A = memSelect << 20 + offset
-	unsigned char* pM;
-	//	unsigned char cdata;
-	//	unsigned short int sdata;
-	//	unsigned int idata;
-
-	if (nRW > 1 || S > 2) {
-		printf("Error: Invalid range of nRW or S"); fflush(stdout);
-		exit(1);
-	}
-
-	memSelect = A >> 20;
-	offset = A & 0xFFFFF;
-	if (memSelect == 0x004) pM = progMEM;         // program memory
-	else if (memSelect == 0x100) pM = dataMEM;  // data memory
-	else if (memSelect == 0x7FF) pM = stakMEM;  // stack
-	else {
-		printf("Error: No memory with address: %08x\n", A); fflush(stdout);
-		exit(1);
-	}
-
-	if (S == 0) {  // byte
-		if (nRW == 0) {	// read
-			return pM[offset];
-		}
-		else {	// write
-			pM[offset] = (unsigned char)V;
-			return 1;
-		}
-	}
-	else if (S == 1) { // half word
-		offset = offset & 0xfffffffe;	// for aligned access
-		if (nRW == 0) {	// read
-			return (pM[offset] << 8) + pM[offset + 1];
-		}
-		else {	// write
-			pM[offset] = (unsigned char)((V >> 8) & 0xff);
-			pM[offset + 1] = (unsigned char)(V & 0xff);
-			return 1;
-		}
-	}
-	else if (S == 2) { // word
-		offset = offset & 0xfffffffc;	// for aligned access
-		if (nRW == 0) {	// read
-			return (pM[offset] << 24) + (pM[offset + 1] << 16) + (pM[offset + 2] << 8) + pM[offset + 3];
-		}
-		else {	// write
-			pM[offset] = (unsigned char)((V >> 24) & 0xff);
-			pM[offset + 1] = (unsigned char)((V >> 16) & 0xff);
-			pM[offset + 2] = (unsigned char)((V >> 8) & 0xff);
-			pM[offset + 3] = (unsigned char)(V & 0xff);
-			return 1;
-		}
-	}
-	return 0;
-}
-
-void resetMem(void)
-{
-	int i;
-	for (i = 0; i < M_SIZE; i++) {
-		progMEM[i] = 0;
-		dataMEM[i] = 0;
-		stakMEM[i] = 0;
-	}
-}
+#include <stdio.h>
+#include <stdlib.h>
 
 unsigned int invertEndian(unsigned int data)
 {
@@ -98,6 +12,7 @@ unsigned int invertEndian(unsigned int data)
 
 	return *(unsigned int*)c;
 }
+
 
 // 바이너리 파일을 읽어서 memory에 loading 하고  PC, SP 를 초기화하는 함수
 void loading(char* bFileName) { 
@@ -143,3 +58,72 @@ void loading(char* bFileName) {
 	SP = STAK_END;
 
 }
+
+// memory.c 사용, register.c 사용 
+// 바이너리 파일을 읽어서 memory에 loading 하고  PC, SP를 초기화하는 함수
+void loading(char* bFileName) { 
+	FILE* bFile;
+	errno_t err;
+
+	unsigned int data;
+	unsigned int addr;
+
+	unsigned int iCount; // # of instructions
+	unsigned int dCount; // # of data
+
+	err = fopen_s(&bFile, bFileName, "rb");
+	if (err) {
+		printf("[DEBUG] 바이너리 파일을 열 수 없음\n");
+		return;
+	}
+
+	fread(&data, sizeof(data), 1, bFile);
+	iCount = invertEndian(data); 
+	fread(&data, sizeof(data), 1, bFile);
+	dCount = invertEndian(data);
+
+	resetMem();
+	
+	addr = 0x00400000;
+	for (int i = 0; i < (int)iCount; i++) {
+		fread(&data, sizeof(unsigned int), 1, bFile);
+		data = invertEndian(data);
+		printf("\n");
+		MEM(addr, data, WRITE, WORD);
+		addr += 4;
+	}
+
+	addr = 0x10000000;
+	for (int i = 0; i < (int)dCount; i++) {
+		fread(&data, sizeof(unsigned int), 1, bFile);
+		data = invertEndian(data);
+		MEM(addr, data, WRITE, WORD);
+		addr += 4;
+	}
+
+	PC = 0x00400000;
+	SP = 0x80000000;
+
+	// 디버깅을  위한 코드
+	addr = 0x00400000;
+	for (int i = 0; i < (int)iCount; i++) {
+		data = MEM(addr, 0, READ, WORD);
+		printf("(test) PROG WORD RD: A=%08x, %02x\n", addr, data);
+		addr += 4;
+	}
+	printf("\n");
+	addr = 0x10000000;
+	for (int i = 0; i < (int)dCount; i++) {
+		data = MEM(addr, 0, READ, WORD);
+		printf("(test) DATA WORD RD: A=%08x, %02x\n", addr, data);
+		addr += 4;
+	}
+	
+}
+
+
+int main(void) {
+	loading("as_ex01_arith.bin");
+	return 0;
+}
+
